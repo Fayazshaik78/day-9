9ff860ac49130ffece750f304b69ebc969397114
Fayaz github <shaikfayazbaba321@gmail.com>
2025-05-14 17:00:32 +0530

    Add files via upload

---
+++ FiveNum.java
@@ -0,0 +1,9 @@
+package com.loops;
+
+public class FiveNum {
+	public static void main(String[] args) {
+		for(int i = 5 ; i<=50 ; i =i+5) {
+		System.out.println(i);
+	}
+
+}}
---
+++ OddNum.java
@@ -0,0 +1,10 @@
+package com.loops;
+
+public class OddNum {
+	public static void main(String[] args) {
+		for(int i=1; i<=15 ; i=i+2) {
+			System.out.println(i);
+		}
+	}
+
+}
